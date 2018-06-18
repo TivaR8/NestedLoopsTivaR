702e2926b77b0754e75aae2201f4149a820dd2a5
TivaR8 <tiva.rait@ocsbstudent.ca>
2018-06-17 21:05:53 -0400

    Initial commit

---
+++ NestedLoopsTivaR/NestedLoopsForm.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace NestedLoopsTivaR
+{
+    partial class frmNestedLoops
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lstNumbers = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(134, 49);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lstNumbers
+            // 
+            this.lstNumbers.FormattingEnabled = true;
+            this.lstNumbers.Location = new System.Drawing.Point(82, 113);
+            this.lstNumbers.Name = "lstNumbers";
+            this.lstNumbers.Size = new System.Drawing.Size(187, 147);
+            this.lstNumbers.TabIndex = 1;
+            // 
+            // frmNestedLoops
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 324);
+            this.Controls.Add(this.lstNumbers);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmNestedLoops";
+            this.Text = "Number Nested Loops By Tiva R";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lstNumbers;
+    }
+}
+
---
+++ NestedLoopsTivaR/NestedLoopsForm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+ * Created by: Tiva Rait
+ * Created on: 17-06-2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #25 - Numbers Nested Loops
+ * This program loops through the numbers through one loop then enters another
+*/
+
+namespace NestedLoopsTivaR
+{
+    public partial class frmNestedLoops : Form
+    {
+        public frmNestedLoops()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // Declare local variables
+            int firstNumber;
+            int secondNumber;
+
+            // clear listbox
+            this.lstNumbers.Items.Clear();
+
+            // Loop through the numbers from 0 to 10
+            for (firstNumber = 0; firstNumber <= 10; firstNumber++)
+            {
+                // For each of the numbers above, loop through again writing
+                //beside it the second number from 0 to 10
+                for (secondNumber = 0; secondNumber <= 10; secondNumber++)
+                {
+                    this.lstNumbers.Items.Add(firstNumber + " -> " + secondNumber);
+                }
+            }
+        }
+    }
+}
